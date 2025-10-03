d91e2ab1e301e77fcd7173802aa4b7601829cf5d
Loi Tran <88891057+loi9@users.noreply.github.com>
2025-10-03 09:04:18 +0700

    server.js

--- backend/server.js
+++ backend/server.js
@@ -20,4 +20,5 @@ mongoose.connect(process.env.MONGO_URI)
 app.use('/api', apiRoutes);
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
