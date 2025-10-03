762c24276e2a4a8c8b984701c1f4b507ced8f156
Loi Tran <88891057+loi9@users.noreply.github.com>
2025-10-03 11:16:30 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ const apiRoutes = require('./routes/api');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST, "PUT", "DELETE"],
+    credentials: false
+}));
 app.use(express.json());
 
 // Database Connection
@@ -22,3 +26,4 @@ app.use('/api', apiRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
