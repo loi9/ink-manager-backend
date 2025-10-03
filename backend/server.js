3cb54b182139bab9e92d8540232402e38433f096
Loi Tran <88891057+loi9@users.noreply.github.com>
2025-10-03 16:37:45 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,12 +25,4 @@ app.use('/api', apiRoutes);
 
 // ================== Start server ==================
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-    console.log(`Backend URL: ${process.env.FRONTEND_URL || "http://localhost:" + PORT}`);
-});
-
-
-
-
-
+app.listen(PORT, () => console.log(Server running on port ${PORT}));
