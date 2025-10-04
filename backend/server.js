0d9f48db8df82f2ccf5e8f05db624d61591e74fe
Loi Tran <88891057+loi9@users.noreply.github.com>
2025-10-04 09:17:53 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,5 +25,6 @@ app.use('/api', apiRoutes);
 
 // ================== Start server ==================
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log('Server running on port ${PORT}'));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
